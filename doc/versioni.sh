296afb552c2cd2d89a6a1ac05d3111db58abece1
oigroig <giorgio.giuffre@studenti.unipd.it>
2016-12-26 17:22:12 +0100

    aggiunta macro per il glossario e aggiustate macro per le versioni

--- doc/versioni.sh
+++ doc/versioni.sh
@@ -1,28 +1,28 @@
-# Script di shell che aggiorna util/vers.sty fornendogli i nomi dei documenti con l'ultima versione disponibile
+# Script di shell che aggiorna util/hx-vers.sty fornendogli i nomi dei documenti con l'ultima versione disponibile
 # da invocare tramite il comando ./versioni.sh dalla directory doc
 
-echo -n "" > util/vers.sty
+echo -n "" > util/hx-vers.sty
 
-echo -n "\newcommand{\NdP}{" >> util/vers.sty
-basename Interni/*/Norme*.tex | sed 's/\(.*\)\..*/\1/' | xargs echo -n >> util/vers.sty
-echo "}" >> util/vers.sty
+echo -n "\newcommand{\NdP}{" >> util/hx-vers.sty
+basename Interni/*/Norme*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g' >> util/hx-vers.sty
+echo "}" >> util/hx-vers.sty
 
-echo -n "\newcommand{\Verbali}{" >> util/vers.sty
-basename Esterni/*/Verbali*.tex | sed 's/\(.*\)\..*/\1/' | xargs echo -n >> util/vers.sty
-echo "}" >> util/vers.sty
+echo -n "\newcommand{\Verbali}{" >> util/hx-vers.sty
+basename Esterni/*/Verbali*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g' >> util/hx-vers.sty
+echo "}" >> util/hx-vers.sty
 
-echo -n "\newcommand{\SdF}{" >> util/vers.sty
-basename Interni/*/Studio*.tex | sed 's/\(.*\)\..*/\1/' | xargs echo -n >> util/vers.sty
-echo "}" >> util/vers.sty
+echo -n "\newcommand{\SdF}{" >> util/hx-vers.sty
+basename Interni/*/Studio*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g' >> util/hx-vers.sty
+echo "}" >> util/hx-vers.sty
 
-echo -n "\newcommand{\PdP}{" >> util/vers.sty
-basename Esterni/*/Piano_di_progetto*.tex | sed 's/\(.*\)\..*/\1/' | xargs echo -n >> util/vers.sty
-echo "}" >> util/vers.sty
+echo -n "\newcommand{\PdP}{" >> util/hx-vers.sty
+basename Esterni/*/Piano_di_progetto*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g' >> util/hx-vers.sty
+echo "}" >> util/hx-vers.sty
 
-echo -n "\newcommand{\PdQ}{" >> util/vers.sty
-basename Esterni/*/Piano_di_qualifica*.tex | sed 's/\(.*\)\..*/\1/' | xargs echo -n >> util/vers.sty
-echo "}" >> util/vers.sty
+echo -n "\newcommand{\PdQ}{" >> util/hx-vers.sty
+basename Esterni/*/Piano_di_qualifica*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g' >> util/hx-vers.sty
+echo "}" >> util/hx-vers.sty
 
-echo -n "\newcommand{\Glossario}{" >> util/vers.sty
-basename Esterni/*/Glossario*.tex | sed 's/\(.*\)\..*/\1/' | xargs echo -n >> util/vers.sty
-echo "}" >> util/vers.sty
+echo -n "\newcommand{\Glossario}{" >> util/hx-vers.sty
+basename Esterni/*/Glossario*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g' >> util/hx-vers.sty
+echo "}" >> util/hx-vers.sty
