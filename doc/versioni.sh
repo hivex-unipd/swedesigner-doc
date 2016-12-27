1b0a0a5e80c86694721ad74f3229828bdea7dcc6
oigroig <giorgio.giuffre@studenti.unipd.it>
2016-12-27 12:57:15 +0100

    creato template di documento, template per diario delle modifiche e aggiustato cose..

--- doc/versioni.sh
+++ doc/versioni.sh
@@ -2,27 +2,31 @@
 # da invocare tramite il comando ./versioni.sh dalla directory doc
 
 echo -n "" > util/hx-vers.sty
+echo "% Ultime versioni dei documenti" >> util/hx-vers.sty
+echo "% da usare tramite il comando \input{../../util/hx-vers}" >> util/hx-vers.sty
+echo "% da aggiornare con il comando ./versioni.sh (dalla cartella doc)" >> util/hx-vers.sty
+echo "" >> util/hx-vers.sty
 
 echo -n "\newcommand{\NdP}{" >> util/hx-vers.sty
-basename Interni/*/Norme*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g' >> util/hx-vers.sty
-echo "}" >> util/hx-vers.sty
+OUTPUT="$(basename Interni/*/Norme*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
+echo "${OUTPUT}}" >> util/hx-vers.sty
 
 echo -n "\newcommand{\Verbali}{" >> util/hx-vers.sty
-basename Esterni/*/Verbali*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g' >> util/hx-vers.sty
-echo "}" >> util/hx-vers.sty
+OUTPUT="$(basename Esterni/*/Verbali*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
+echo "${OUTPUT}}" >> util/hx-vers.sty
 
 echo -n "\newcommand{\SdF}{" >> util/hx-vers.sty
-basename Interni/*/Studio*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g' >> util/hx-vers.sty
-echo "}" >> util/hx-vers.sty
+OUTPUT="$(basename Interni/*/Studio*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
+echo "${OUTPUT}}" >> util/hx-vers.sty
 
 echo -n "\newcommand{\PdP}{" >> util/hx-vers.sty
-basename Esterni/*/Piano_di_progetto*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g' >> util/hx-vers.sty
-echo "}" >> util/hx-vers.sty
+OUTPUT="$(basename Esterni/*/Piano_di_progetto*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
+echo "${OUTPUT}}" >> util/hx-vers.sty
 
 echo -n "\newcommand{\PdQ}{" >> util/hx-vers.sty
-basename Esterni/*/Piano_di_qualifica*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g' >> util/hx-vers.sty
-echo "}" >> util/hx-vers.sty
+OUTPUT="$(basename Esterni/*/Piano_di_qualifica*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
+echo "${OUTPUT}}" >> util/hx-vers.sty
 
 echo -n "\newcommand{\Glossario}{" >> util/hx-vers.sty
-basename Esterni/*/Glossario*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g' >> util/hx-vers.sty
-echo "}" >> util/hx-vers.sty
+OUTPUT="$(basename Esterni/*/Glossario*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
+echo "${OUTPUT}}" >> util/hx-vers.sty
