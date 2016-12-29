4955d0be5768158d1e3d22ea0a330a66f4fa8a8c
oigroig <giorgio.giuffre@studenti.unipd.it>
2016-12-29 12:58:13 +0100

    riordinata la struttura delle norme di progetto

--- doc/versioni.sh
+++ doc/versioni.sh
@@ -11,46 +11,46 @@ echo "" >> util/hx-vers.sty
 
 echo -n "\newcommand{\NdP}{" >> util/hx-vers.sty
 OUTPUT="$(basename Interni/*/Norme*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
-echo "${OUTPUT}}" >> util/hx-vers.sty
+echo "${OUTPUT}.pdf}" >> util/hx-vers.sty
 
 echo -n "\newcommand{\Verbali}{" >> util/hx-vers.sty
 OUTPUT="$(basename Esterni/*/Verbali*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
-echo "${OUTPUT}}" >> util/hx-vers.sty
+echo "${OUTPUT}.pdf}" >> util/hx-vers.sty
 
 echo -n "\newcommand{\AdR}{" >> util/hx-vers.sty
 OUTPUT="$(basename Esterni/*/Analisi*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
-echo "${OUTPUT}}" >> util/hx-vers.sty
+echo "${OUTPUT}.pdf}" >> util/hx-vers.sty
 
 echo -n "\newcommand{\SdF}{" >> util/hx-vers.sty
 OUTPUT="$(basename Interni/*/Studio*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
-echo "${OUTPUT}}" >> util/hx-vers.sty
+echo "${OUTPUT}.pdf}" >> util/hx-vers.sty
 
 echo -n "\newcommand{\PdP}{" >> util/hx-vers.sty
 OUTPUT="$(basename Esterni/*/Piano_di_progetto*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
-echo "${OUTPUT}}" >> util/hx-vers.sty
+echo "${OUTPUT}.pdf}" >> util/hx-vers.sty
 
 echo -n "\newcommand{\PdQ}{" >> util/hx-vers.sty
 OUTPUT="$(basename Esterni/*/Piano_di_qualifica*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
-echo "${OUTPUT}}" >> util/hx-vers.sty
+echo "${OUTPUT}.pdf}" >> util/hx-vers.sty
 
 echo -n "\newcommand{\Glossario}{" >> util/hx-vers.sty
 OUTPUT="$(basename Esterni/*/Glossario*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
-echo "${OUTPUT}}" >> util/hx-vers.sty
+echo "${OUTPUT}.pdf}" >> util/hx-vers.sty
 
 if [[ -f Esterni/*/Specifica_tecnica*.tex ]]; then
 echo -n "\newcommand{\ST}{" >> util/hx-vers.sty
 OUTPUT="$(basename Esterni/*/Specifica_tecnica*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
-echo "${OUTPUT}}" >> util/hx-vers.sty
+echo "${OUTPUT}.pdf}" >> util/hx-vers.sty
 fi
 
 if [[ -f Esterni/*/Definizione*.tex ]]; then
 echo -n "\newcommand{\DdP}{" >> util/hx-vers.sty
 OUTPUT="$(basename Esterni/*/Definizione*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
-echo "${OUTPUT}}" >> util/hx-vers.sty
+echo "${OUTPUT}.pdf}" >> util/hx-vers.sty
 fi
 
 if [[ -f Esterni/*/Manuale*.tex ]]; then
 echo -n "\newcommand{\MU}{" >> util/hx-vers.sty
 OUTPUT="$(basename Esterni/*/Manuale*.tex | sed 's/\(.*\)\..*/\1/' | sed 's/_/\\_/g')"
-echo "${OUTPUT}}" >> util/hx-vers.sty
+echo "${OUTPUT}.pdf}" >> util/hx-vers.sty
 fi
