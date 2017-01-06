58320cc36d4ace72c534371c7f8aabbd300775d1
oigroig <giorgio.giuffre@studenti.unipd.it>
2017-01-06 14:23:08 +0100

    problemi di codifica utf8; script gloss.sh

--- doc/Esterni/Glossario/gloss.sh
+++ doc/Esterni/Glossario/gloss.sh
@@ -1,10 +1,36 @@
-# verifica che non ci siano termini inutili nel glossario
-
-echo "termini inutilizzati:"
-cat terminiGlossario.tex | grep name\= | while read -r MATCH ; do
-	MATCH=`echo $MATCH | sed 's/name={//' | sed 's/},//'`
-	OK=`grep -i "gloss{${MATCH}}" ../../*/*/*.tex`
-	if [[ ! $OK ]]; then
-		echo "${MATCH}"
+# verifica che il glossario non contenga termini inutili:
+printf "\n\ntermini inutilizzati:\n"
+cat terminiGlossario.tex | grep \\item | while read -r MATCH ; do
+	MATCH=`echo $MATCH | sed 's/\\\item\[//' | sed 's/\].*//'`
+	OK=false
+	for f in ../../*/*/*.tex; do
+		if [[ `grep -i "gloss{${MATCH}}" $f` ]]; then
+			OK=true
+		fi
+	done
+	if [ "$OK" = false ]; then
+		echo -n "${MATCH}, "
 	fi
 done
+echo -e "\b\b "
+
+
+
+# verifica che al glossario non manchi qualche termine utilizzato:
+# (l'opzione -w fornisce il path del file
+# con il termine da inserire in glossario)
+printf "\n\ntermini da inserire:\n"
+for f in ../../*/*/*.tex; do
+	cat $f | grep -o "gloss{.*}" | while read -r MATCH ; do
+		MATCH=`echo $MATCH | sed 's/gloss{//' | sed 's/}.*//'`
+		OK=false
+		if [[ ! `grep -i "item\[${MATCH}\]" terminiGlossario.tex` ]]; then
+			if [[ $1 = "-w" ]]; then
+				echo "${MATCH} (${f})"
+			else
+				echo -n "${MATCH}, "
+			fi
+		fi
+	done
+done
+echo -e "\b\b "
