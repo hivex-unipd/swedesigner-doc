6bf750c0a9ab4d2fc0db868d05caa25650c07c27
oigroig <giorgio.giuffre@studenti.unipd.it>
2017-01-11 00:37:56 +0100

    glossario

--- doc/Esterni/Glossario/alfa.sh
+++ doc/Esterni/Glossario/alfa.sh
@@ -1,5 +1,6 @@
 # implementa la paginazione alfabetica del glossario:
+# BOZZA: NON FUNZIONA NULLA
 alfabeto='a b c d e f g h i j k l m n o p q r s t u v w x y z'
 for c in $alfabeto; do
-	cat terminiGlossario.tex | sed 's/item\[a/a/'
+	cat terminiGlossario.tex | awk '!x{x=sub("\item","a\item")}7'
 done
